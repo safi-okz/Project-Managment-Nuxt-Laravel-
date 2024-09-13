2182d3fa8e3b70a9016d090ddd98f332e6cd385d
safi-okz <safi90655@gmail.com>
2024-09-13 22:07:00 +0500

    project ui start

--- frontend/pages/dashboard/projects/index.vue
+++ frontend/pages/dashboard/projects/index.vue
@@ -5,7 +5,15 @@
 </template>
 
 <script setup>
+const { api } = useAxios();
 
+const projects = ref([]);
+onMounted(async () => await projectsLoad());
+
+const projectsLoad = async () => {
+      const response = await api.get('/api/projects');
+      console.log('prjects response ', response);
+}
 </script>
 
 <style>
